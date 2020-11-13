6a5601a778aa9286addd59c2a7366d444cdbe714
Chubo Zeko <chubozeko@gmail.com>
2020-11-13 16:23:17 +0200

    Initial Upload
    - Added Player script
    - Added CameraMovement script
    - Added temporary sprites

---
+++ IF (NNGJ2020)/Assets/Scripts/Player.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+    
+    public float speed = 5f;
+    public bool isFacingRight = true;
+    private float wHeight;
+    private float health = 100f;
+
+    public float jumpSpeed = 7f;
+    public bool isJumping = false;
+    private float jumpButtonPressTime = 0f;
+    private float maxJumpTime = 0.2f;
+
+    private float rayCastLength = 0.005f;
+
+    void Start()
+    {
+        health = 100f;
+    }
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+
+        // wHeight: the bottom part of the Player
+        wHeight = GetComponent<Collider2D>().bounds.extents.y + 0.1f;
+    }
+
+
+    void FixedUpdate()
+    {
+        /*** HORIZONTAL MOVEMENT ***/
+        // Get Horizontal Movement (Walking)
+        float horzMove = Input.GetAxisRaw("Horizontal");
+
+        // Move Player according to direction
+        Vector2 vect = rb.velocity;
+        rb.velocity = new Vector2(horzMove * speed, vect.y);
+
+        // Check movement direction and make player face towards direction
+        FlipPlayer(horzMove);
+
+        /*** VERTICAL MOVEMENT ***/
+        // Get Vertical Movement (Jumping)
+        float vertMove = Input.GetAxisRaw("Vertical");
+        if (IsOnGround() && isJumping == false)
+        {
+            if (vertMove > 0f)
+            {
+                isJumping = true;
+            }
+        }
+
+        // Check if Player is busy jumping & jump time hasn't elapsed yet
+        if (isJumping && jumpButtonPressTime < maxJumpTime)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+
+        // Check if Player is still busy jumping
+        if (vertMove >= 1f)
+        {
+            jumpButtonPressTime += Time.deltaTime;
+        }
+        else
+        {
+            isJumping = false;
+            jumpButtonPressTime = 0f;
+        }
+
+        // Check if Jumping time period has elapsed
+        if (jumpButtonPressTime > maxJumpTime)
+        {
+            vertMove = 0f;
+        }
+    }
+
+    private void FlipPlayer(float horzMove)
+    {
+        // Face RIGHT
+        if (horzMove > 0 && !isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+            sr.flipX = !sr.flipX;
+        }
+        // Face LEFT
+        if (horzMove < 0 && isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+            sr.flipX = !sr.flipX;
+        }
+    }
+
+    public bool IsOnGround()
+    {
+        bool groundCheck = Physics2D.Raycast(
+            new Vector2(transform.position.x, transform.position.y - wHeight),
+            -Vector2.up,
+            rayCastLength);
+
+        if (groundCheck) return true;
+        else return false;
+    }
+
+    public void GetHit(float damage)
+    {
+        health -= damage;
+        if (health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ IF (NNGJ2020)/Assets/Scripts/PlayerCameraMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraMovement : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public float cameraSpeed;
+    public bool enableHorizontalCameraMovement;
+    public bool enableVerticalCameraMovement;
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+
+    private void Start()
+    {
+        minX = 0;
+        maxX = 8;
+        minY = 0;
+        maxY = 8;
+        cameraSpeed = 5;
+    }
+
+    void FixedUpdate()
+    {
+        if (cameraTarget != null)
+        {
+            // Gradually move Camera
+            var newPos = Vector2.Lerp(transform.position,
+            cameraTarget.position,
+            Time.deltaTime * cameraSpeed);
+
+            var vect3 = new Vector3(newPos.x, newPos.y, -10f);
+
+            // Clamp camera position to make the character be in the left side of the screen
+            var clampX = 0f;
+            var clampY = 0f;
+            if (enableHorizontalCameraMovement)
+            {
+                clampX = Mathf.Clamp(vect3.x, minX, maxX);
+            }
+            else
+            {
+                clampX = 0;
+            }
+            if (enableVerticalCameraMovement)
+            {
+                clampY = Mathf.Clamp(vect3.y, minY, maxY);
+            }
+            else
+            {
+                clampY = 0;
+            }
+
+            transform.position = new Vector3(clampX, clampY, -10f);
+        }
+    }
+}
