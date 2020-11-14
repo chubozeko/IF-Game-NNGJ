192b08089016d434504a288e782392f9c687e9d0
MBRGA <thatdeaddude@gmail.com>
2020-11-14 11:33:41 +0200

    Yay color swap works

--- IF (NNGJ2020)/Assets/Scripts/Player.cs
+++ IF (NNGJ2020)/Assets/Scripts/Player.cs
@@ -38,11 +38,7 @@ public class Player : MonoBehaviour
         // wHeight: the bottom part of the Player
         wHeight = GetComponent<Collider2D>().bounds.extents.y + 0.1f;
 
-        InitColorSwapTex();
-
-        for (int i = 1; i <= 255; i++)
-            SwapGreen((byte)i, new Color(1,0,1,1));
-        mColorSwapTex.Apply();
+        sr.material.SetColor("_RepCol", new Color(1.0f, 0.0f, 0.0f, 0.5f));
     }
 
 
@@ -132,25 +128,5 @@ public class Player : MonoBehaviour
         {
             Destroy(gameObject);
         }
-    }
-
-    public void InitColorSwapTex() {
-        Texture2D colorSwapTex = new Texture2D(256, 1, TextureFormat.RGBA32, false, false);
-        colorSwapTex.filterMode = FilterMode.Point;
-
-        for (int i = 0; i < colorSwapTex.width; ++i) 
-            colorSwapTex.SetPixel(i, 0, new Color(0.0f, 0.0f, 0.0f, 0.0f));
-
-        colorSwapTex.Apply();
-
-        sr.material.SetTexture("_SwapTex", colorSwapTex);
-
-        mSpriteColors = new Color[colorSwapTex.width];
-        mColorSwapTex = colorSwapTex;
-    }
-
-    public void SwapGreen(byte index, Color color) {
-        mSpriteColors[index] = color;
-        mColorSwapTex.SetPixel((int)index, 0, color);
     }
 }
