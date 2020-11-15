52c1ad69e9c9384677159a3d5d9d8c1019fb9748
MBRGA <thatdeaddude@gmail.com>
2020-11-15 13:50:54 +0200

    Gas shooter working

---
+++ IF (NNGJ2020)/Assets/Scripts/FixedLifetime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedLifetime : MonoBehaviour
+{
+    public float LifeTime = 5;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Destroy(gameObject, LifeTime);
+    }
+}
