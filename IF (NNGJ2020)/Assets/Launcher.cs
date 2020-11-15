bdadcda2da37c10fa382937cf98164af1e17bef6
joshua <180113@virtualwindow.co.za>
2020-11-15 12:41:14 +0200

    added launcher script pre testing

--- IF (NNGJ2020)/Assets/Launcher.cs
+++ IF (NNGJ2020)/Assets/Launcher.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Launcher : MonoBehaviour
+
 {
+[SerializeField] PlayerCameraMovement _playerCameraMovement;
+[SerializeField] float launchForceValue;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Player")){
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2 (0,))
+        }
     }
 }
